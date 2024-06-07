1cb982743eaf5d3c9c721d647525f5a139c4d408
Mola Dafa <mola_d@hotmail.com>
2024-06-07 16:43:19 +1000

    dfsdf

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,10 +5,7 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/flashcards";
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri, {});
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err);

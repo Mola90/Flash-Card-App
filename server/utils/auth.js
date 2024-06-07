4ff1b025d93ce9a0559d43e13f37e8530703c981
Mola Dafa <mola_d@hotmail.com>
2024-06-07 19:08:45 +1000

    Converted auth.js to ES module syntax and ensured correct import paths

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,41 +1,39 @@
-
-const { GraphQLError } = require('graphql');
-const jwt = require('jsonwebtoken');
+import { GraphQLError } from 'graphql';
+import jwt from 'jsonwebtoken';
 
 const secret = 'Molassecretcode';
 const expiration = '2h';
 
+export const AuthenticationError = new GraphQLError('Could not authenticate user.', {
+  extensions: {
+    code: 'UNAUTHENTICATED',
+  },
+});
 
-module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    },
-  }),
-  authMiddleware: function ({ req }) {
-    console.log("this is a reqestttttttttttttttttttt", req.headers);
-    let token = req.body.token || req.query.token || req.headers.authorization;
+export function authMiddleware({ req }) {
+  console.log("this is a reqestttttttttttttttttttt", req.headers);
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
 
-    if (!token) {
-      return req;
-    }
-
-    try {
-      const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = authenticatedPerson;
-
-    } catch {
-      console.log('Invalid token');
-    }
-
+  if (!token) {
     return req;
-  },
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
-    return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
-  },
-};
+  }
+
+  try {
+    const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = authenticatedPerson;
+
+  } catch {
+    console.log('Invalid token');
+  }
+
+  return req;
+}
+
+export function signToken({ email, name, _id }) {
+  const payload = { email, name, _id };
+  return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
+}
